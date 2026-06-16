d70e45ec251205824bdf2469cfe1e353059633bc
Christine Grimadeau <cgrimad1@yahoo.com>
2026-06-16 11:51:38 -0400

    Added Assets, Flashcard comp, etc

--- BrewCards/src/App.jsx
+++ BrewCards/src/App.jsx
@@ -1,1 +1,12 @@
+import './App.css'
 
+export default function App() {
+  return (
+    <div className="container">
+      <h2>Brew Cards</h2>
+      <h4>The quiz for caffeine lovers!</h4>
+      <h5>Number of Cards: 10</h5>
+      <br/>
+    </div>
+  )
+}
---
+++ BrewCards/src/Flashcard/Flashcard.jsx
@@ -0,0 +1,1 @@
+import './Flashcard.css'
